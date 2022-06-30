6946eeceb221623bd69d4577eaa13eb9e9b8e0f5
EviLCaT12 <kuznetsov.timofey.2003@mail.ru>
2022-06-30 23:51:21 +1000

    add header, install bootstrap(second try)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,21 @@
 <template>
-  <h1>РУся привет</h1>
+ <div class="#app">
+   <nav-bar></nav-bar>
+ </div>
 </template>
 
 <script>
 
 
+import NavBar from "@/components/NavBar";
 export default {
   name: 'App',
   components: {
+    NavBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
