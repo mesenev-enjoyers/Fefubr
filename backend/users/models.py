01017928655bbff80b4453908ffb036f86e65bff
Simoffsky <maks-kirsanov26@mail.ru>
2022-06-30 17:37:27 +1000

    added user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    rating = models.IntegerField(default=0)
+    status = models.CharField(max_length=255)
+    avatar = models.ImageField(default='default_avatar.png', upload_to='avatars')
+
+    def __str__(self):
+        return self.username
+
+
