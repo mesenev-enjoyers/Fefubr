d2f2fd10a9a3efb37d11bfa87dae50b282d9219e
EviLCaT12 <kuznetsov.timofey.2003@mail.ru>
2022-07-04 18:00:30 +1000

    fixed bugs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from "@/router";
+import router from "@/router/router";
 const app = createApp(App);
-app.use(router);
-app.mount('#app');
+app
+    .use(router)
+    .mount('#app');
 
 
