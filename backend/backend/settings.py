7bda0247f271a033738cffbb5c77008b3e4ef749
Simoffsky <78417209+Simoffsky@users.noreply.github.com>
2022-07-07 14:36:51 +1000

    removed password validation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,6 +121,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # media/ ?
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_PASSWORD_VALIDATORS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
