e603654cbba0310405bf4882e273857e11112407
EviLCaT12 <kuznetsov.timofey.2003@mail.ru>
2022-07-08 23:01:24 +1000

    add some chat

--- frontend/src/Pages/UserChats.vue
+++ frontend/src/Pages/UserChats.vue
@@ -1,10 +1,21 @@
 <template>
- <div class="root"></div>
+ <div class="root">
+   <nav-bar></nav-bar>
+   <div class="container-fluid d-flex h-100 justify-content-center align-items-center p-0 mt-15" id="pictures">
+     <a href="https://t.me/Silly_bot_bot"><h2>Переходите в нашего телеграм бота</h2></a>
+   </div>
+   <div class="container-fluid d-flex h-100 justify-content-center align-items-center p-0 mt-15" id="pictures">
+     <img src="https://i.imgur.com/FYjJbTP.png">
+   </div>
+ </div>
 </template>
 
 <script>
+import NavBar from "@/components/UI/NavBar";
 export default {
-  name: "UserChats"
+  name: "UserChats",
+  components: {NavBar}
+
 }
 </script>
 
